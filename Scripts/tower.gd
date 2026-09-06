362c705215169c7e1c2aa0cb3bd0aa6d689bfcb2
ddottee <jamesjrdotten@gmail.com>
2026-09-06 15:18:58 -0400

    added tower functions + upgrade panel

--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -1,11 +1,19 @@
+extends StaticBody2D
 class_name Tower
 
-enum targetTypes {FIRST, LAST, NEAREST}
+@onready var rangeCollider: CollisionShape2D = $rangeArea/rangeCollider
+@onready var rangeArea: Area2D = $rangeArea
+@onready var shotTimer: Timer = $shotTimer
+@onready var bulletContainer: Node = $bulletContainer
+@onready var aimMark: Marker2D = $aimMark
+enum targetTypes {FIRST, LAST}
 
 @export_group("Stats")
 
+
 @export var damage := 1
-@export var targetRange := 1
+@export var bulletType : PackedScene
+@export var targetRange := 100
 @export var projectileSpeed := 1
 
 
@@ -15,7 +23,72 @@ enum targetTypes {FIRST, LAST, NEAREST}
 @export var canTargetAir := false
 
 
-
 @export_group("Costs")
 @export var cost := 1
 @export var upgradeCost := 1
+
+
+var currTargets = []
+var curr
+var pathName
+var startShoot := false
+
+func _ready() -> void:
+	rangeArea.body_entered.connect(_on_range_area_body_entered)
+	rangeArea.body_exited.connect(_on_range_area_body_exited)
+	rangeCollider.shape.set_deferred("radius", targetRange)
+
+func _process(delta: float) -> void:
+	if is_instance_valid(curr):
+		self.look_at(curr.global_position)
+		if shotTimer.is_stopped():
+			Shoot()
+			shotTimer.start()
+	else:
+		for i in get_node("bulletContainer").get_child_count():
+			get_node("bulletContainer").get_child(i).queue_free()
+	updateStats()
+
+
+func Shoot():
+	var tempShot = bulletType.instantiate()
+	tempShot.pathName = pathName
+	tempShot.bulletDamage = damage
+	get_node("bulletContainer").add_child(tempShot)
+	tempShot.global_position = aimMark.global_position
+
+func updateStats():
+	pass
+
+
+
+func _on_range_area_body_entered(body: Node2D) -> void:
+	if body.is_class("Enemy"):
+		var tempArr = []
+		currTargets = get_node("rangeArea").get_overlapping_bodies()
+		for i in currTargets:
+			if i.is_class("Enemy"):
+				tempArr.append(i)
+		
+		var currTarget = null
+		
+		for i in tempArr:
+			if currTarget == null:
+				currTarget = i.get_node("../")
+			else:
+				if priorityTarget == targetTypes.FIRST:
+					if i.get_parent().get_progress() > currTarget.get_progress():
+						currTarget = i.get_node("../")
+				else:
+					if i.get_parent().get_progress() < currTarget.get_progress():
+						currTarget = i.get_node("../")
+				curr = currTarget
+				pathName = currTarget.get_parent().name
+
+func _on_range_area_body_exited(body: Node2D) -> void:
+	currTargets = get_node("rangeArea").get_overlapping_bodies()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_mask == 1:
+		
+		pass
