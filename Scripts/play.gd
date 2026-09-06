ab8cac4cbb02e2658b54b189ce64cdfda33f3113
ddottee <jamesjrdotten@gmail.com>
2026-09-06 12:13:42 -0400

    ruined level 1

--- Scripts/play.gd
+++ Scripts/play.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-@onready var level = preload("res://Scenes/levels.tscn")
+@onready var level = preload("res://Scenes/level_one.tscn")
 func _on_pressed() -> void:
 	GlobalScript.gameState = GlobalScript.gameStates.PAUSE
-	get_tree().change_scene_to_file("res://Scenes/levels.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_one.tscn")
